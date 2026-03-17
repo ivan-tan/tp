2c7e7fa3c557d9b7a72b2a2b5fc9aee5d285d9a7
ivan-tan <tanjunhaoivan@gmail.com>
2026-03-17 21:33:10 +0800

    Edit help command

--- src/main/java/seedu/duke/HelpCommand.java
+++ src/main/java/seedu/duke/HelpCommand.java
@@ -3,11 +3,11 @@ package seedu.duke;
 public class HelpCommand extends Command {
     @Override
     public void execute(ExpenseManager expenses, UI ui) throws ExpensiveLehException {
-        String helpMessage = "To add an expense, use 'add cat/CATEGORY n/NAME val/VALUE [date/DATE]' "
+        String helpMessage = "To add an expense, use 'add cat/CATEGORY n/NAME a/AMOUNT [date/DATE]' "
                 + "Eg: add cat/Food n/Jollibee val/9.95\n"
                 + "To edit an expense, use 'edit INDEX [cat/CATEGORY] [n/NAME] [val/VALUE] [date/DATE]'\n"
                 + "To list all expenses and view remaining budget, use 'list'\n"
-                + "To set a budget, use 'budget set val/VALUE'. Eg: budget set val/$1000\n"
+                + "To set a budget, use 'budget VALUE'. Eg: budget 1000\n"
                 + "To delete an expense, use 'delete INDEX'. Eg: delete 3\n"
                 + "To exit the program, use 'exit'";
 
